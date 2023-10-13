e3ac1618c60d12ef04bf6d1b59f0c833368c5dcd
amevide998 <hdsvidi@gmail.com>
2023-10-13 13:23:28 +0700

    initial project

---
+++ src/main/java/com/hdscode/kadoblitz/KadoblitzApplication.java
@@ -0,0 +1,13 @@
+package com.hdscode.kadoblitz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KadoblitzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KadoblitzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hdscode/kadoblitz/KadoblitzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdscode.kadoblitz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KadoblitzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
